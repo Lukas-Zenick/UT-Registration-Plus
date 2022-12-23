99ce3c5a641bfe3d10130476dbfdfa1978062985
Lukas Zenick <lukas@utexas.edu>
2022-12-23 17:32:07 -0600

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -207,6 +207,7 @@ $("#export-class").click(function () {
 
 function openSearch(semester, department, level, courseCode) {
     var link = "";
+    console.log(courseCode)
     if (courseCode) {
         link = `https://utdirect.utexas.edu/apps/registrar/course_schedule/${semester}/results/?search_type_main=COURSE&fos_cn=${department}&course_number=${courseCode}`;
     } else {
